56168401bf428181de3e53ac10afd31114038272
anushaka23 <anushaka.underpinservices@gmail.com>
2024-03-21 11:13:36 +0530

    middleware err

--- src/middleware.js
+++ src/middleware.js
@@ -1,43 +1,40 @@
 import { NextResponse } from "next/server";
-import Cookies from "js-cookie";
-export default function middleware(req) {
-  const loggedin = Cookies.get("token");
-  console.log(loggedin);
-  const role = req.cookies.get("role");
-  const { pathname } = req.nextUrl;
+import jwt from "jsonwebtoken";
 
+export default function middleware(req) {
+  const loggedin = req.cookies.get("token");
+  const { pathname } = req.nextUrl;
+//
   if (!loggedin && pathname !== "/login") {
     return NextResponse.redirect(new URL("/login", req.url));
   }
-
   if (loggedin && pathname === "/login") {
-    return NextResponse.redirect(new URL("/message", req.url));
+    return NextResponse.redirect(new URL(`/message`, req.url));
   }
 
-  if (loggedin && role && role.value === "admin") {
-    if (
-      pathname === "/agents" ||
-      pathname === "/message" ||
-      pathname === "/create"
-    ) {
-      return NextResponse.next();
+  //
+  if (loggedin) {
+    const decodeToken = jwt.decode(loggedin.value);
+    const role = decodeToken ? decodeToken.role : null;
+    if (role && role === "admin") {
+      if (
+        pathname === "/agents" ||
+        pathname === "/message" ||
+        pathname === "/create"
+      ) {
+        return NextResponse.next();
+      }
+      return NextResponse.redirect(new URL("/message", req.url));
     }
-
-    return NextResponse.redirect(new URL("/message", req.url));
-  }
-
-  if (loggedin && role && role.value === "agent") {
-    if (pathname === "/message" || pathname === "/newmessage") {
-      return NextResponse.next();
+    if (role && role === "agent") {
+      if (pathname === "/message" || pathname === "/newmessage") {
+        return NextResponse.next();
+      }
+      return NextResponse.redirect(new URL("/message", req.url));
     }
-
-    return NextResponse.redirect(new URL("/message", req.url));
   }
-
-  // For any other cases
+  // For any other cases return
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: "/((?!api|static|.\..|_next).*)",
-};
+export const config = { matcher: "/((?!api|static|.\..|_next).*)" };
